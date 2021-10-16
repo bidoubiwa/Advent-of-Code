743d0dee1f2f959d0dbb67b6fd17edca147a1b95
Charlotte Vermandel <charlottevermandel@gmail.com>
2021-10-17 01:43:34 +0200

    Add part 1 of day 7

--- advent2018/day_7/src/bin/part_1.rs
+++ advent2018/day_7/src/bin/part_1.rs
@@ -1,3 +1,50 @@
+use day_7::*;
+use std::collections::HashMap;
+use utils::*;
+
+fn sort_block(block: &mut Vec<&Step>) {
+    block.sort_by_key(|step| step.name);
+}
+
+fn fulfilled_predicate(step: &Step, processed: &Vec<char>) -> bool {
+    let parents = &step.parents;
+    let unfinished: Vec<_> = parents
+        .iter()
+        .filter(|step| !processed.contains(step))
+        .collect();
+    unfinished.is_empty()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let lines = read_lines();
+    let mut steps = HashMap::new();
+    ('A'..='Z').for_each(|letter| {
+        steps.insert(letter, Step::new(letter));
+    });
+    populate_steps(&mut steps, lines);
+    let mut starting_block: Vec<_> = steps
+        .values()
+        .filter(|step| step.parents.is_empty())
+        .collect();
+    sort_block(&mut starting_block);
+    let mut sequence = Vec::new();
+    loop {
+        if starting_block.len() == 0 {
+            break;
+        }
+        let step = starting_block[0];
+        sequence.push(step.name);
+        let childs = &step.childs;
+        childs.iter().for_each(|child| {
+            if fulfilled_predicate(&steps[child], &sequence) {
+                starting_block.push(&steps[child])
+            }
+        });
+        starting_block.remove(0);
+        sort_block(&mut starting_block);
+    }
+    println!(
+        "The complete sequence order is: {}",
+        sequence.iter().collect::<String>()
+    )
 }
--- advent2018/day_7/src/lib.rs
+++ advent2018/day_7/src/lib.rs
@@ -1,14 +1,31 @@
-use std::error::Error;
-use std::str::FromStr;
+use std::collections::HashMap;
 
+#[derive(Debug)]
 pub struct Step {
     pub name: char,
-    pub dependencies: Vec<Step>,
+    pub childs: Vec<char>,
+    pub parents: Vec<char>,
 }
 
-// impl FromStr for Step {
-//     type Err = Box<dyn Error>;
-//     fn from_str(s: &str) -> Result<Self, Self::Err> {
+impl Step {
+    pub fn new(name: char) -> Step {
+        Step {
+            name: name,
+            childs: Vec::new(),
+            parents: Vec::new(),
+        }
+    }
+}
 
-//     }
-// })
+pub fn populate_steps(steps: &mut HashMap<char, Step>, instructions: Vec<String>) {
+    instructions.iter().for_each(|instruction| {
+        let relationship: Vec<&str> = instruction
+            .split(' ')
+            .filter(|slice| slice.len() == 1)
+            .collect();
+        let parent = relationship[0].chars().next().unwrap();
+        let child = relationship[1].chars().next().unwrap();
+        steps.get_mut(&child).unwrap().parents.push(parent);
+        steps.get_mut(&parent).unwrap().childs.push(child);
+    })
+}
